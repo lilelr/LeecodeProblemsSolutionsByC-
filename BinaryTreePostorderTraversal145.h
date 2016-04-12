92c4f3a5ce6f4a3b01baf0b886cc04ce0a903cc4
lilelr <lilelr@163.com>
2016-04-12 14:03:19 +0800

    141 ac

--- BinaryTreePostorderTraversal145.h
+++ BinaryTreePostorderTraversal145.h
@@ -4,5 +4,80 @@
 
 #ifndef LEECODEPROBLEMSSOLUTIONSBYC__BINARYTREEPOSTORDERTRAVERSAL145_H
 #define LEECODEPROBLEMSSOLUTIONSBYC__BINARYTREEPOSTORDERTRAVERSAL145_H
+#include <iostream>
+#include <vector>
+#include <stack>
 
+using namespace std;
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) { }
+};
+
+void preOrder(TreeNode *root, vector<int> &ans) {
+    if (root != nullptr) {
+        ans.push_back(root->val);
+        preOrder(root->left, ans);
+        preOrder(root->right, ans);
+    }
+}
+
+vector<int> preorderTraversal(TreeNode *root) {
+    vector<int> ans;
+    if (root == nullptr) return ans;
+    preOrder(root, ans);
+
+//    for(int i=0;i<ans.size();i++){
+//        cout<<ans[i]<<endl;
+//    }
+    return ans;
+}
+
+
+vector<int> postorderTraversal(TreeNode *root) {
+    vector<int> ans;
+    if (root == NULL) return ans;
+    stack<TreeNode*> stack;
+    TreeNode *p = root;
+    TreeNode *r = NULL;
+    while (p || !stack.empty()) {
+        if (p) {
+            stack.push(p);
+            p = p->left;
+        } else {
+            TreeNode *p = stack.top();
+            if (p->right && p->right != r) {
+                p = p->right;
+                stack.push(p);
+                p = p->left;
+            } else {
+                stack.pop();
+                ans.push_back(p->val);
+                r = p;
+                p = NULL;
+            }
+        }
+    }
+    return ans;
+
+}
+
+int main() {
+    TreeNode *root = new TreeNode(1);
+//    root->left = new TreeNode(2);
+//    root->left->right = new TreeNode(3);
+//    root->right = new TreeNode(6);
+    root->right = new TreeNode(2);
+    root->right->left = new TreeNode(3);
+//    preorderTraversal(root);
+    vector<int> ans = postorderTraversal(root);
+    for (int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << endl;
+    }
+    return 0;
+}
 #endif //LEECODEPROBLEMSSOLUTIONSBYC__BINARYTREEPOSTORDERTRAVERSAL145_H
--- main.cpp
+++ main.cpp
@@ -6,74 +6,37 @@
 
 using namespace std;
 
-//Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
+// Definition for singly-linked list.
+struct ListNode {
+       int val;
+       ListNode *next;
+       ListNode(int x) : val(x), next(NULL) {}
+    };
 
-    TreeNode(int x) : val(x), left(NULL), right(NULL) { }
-};
+bool hasCycle(ListNode *head) {
+    if(head== nullptr || head->next== nullptr ) return false;
 
-void preOrder(TreeNode *root, vector<int> &ans) {
-    if (root != nullptr) {
-        ans.push_back(root->val);
-        preOrder(root->left, ans);
-        preOrder(root->right, ans);
+    ListNode* pre= head->next;
+    ListNode* p = head->next->next;
+    while (p!= nullptr && p->next!= nullptr && p->next->next!= nullptr && p!=pre){
+        p=p->next->next;
+        pre=pre->next;
     }
-}
 
-vector<int> preorderTraversal(TreeNode *root) {
-    vector<int> ans;
-    if (root == nullptr) return ans;
-    preOrder(root, ans);
+    if(p==pre) return true;
+    return false;
 
-//    for(int i=0;i<ans.size();i++){
-//        cout<<ans[i]<<endl;
-//    }
-    return ans;
-}
-
-
-vector<int> postorderTraversal(TreeNode *root) {
-    vector<int> ans;
-    if (root == NULL) return ans;
-    stack<TreeNode*> stack;
-    TreeNode *p = root;
-    TreeNode *r = NULL;
-    while (p || !stack.empty()) {
-        if (p) {
-            stack.push(p);
-            p = p->left;
-        } else {
-            TreeNode *p = stack.top();
-            if (p->right && p->right != r) {
-                p = p->right;
-                stack.push(p);
-                p = p->left;
-            } else {
-                stack.pop();
-                ans.push_back(p->val);
-                r = p;
-                p = NULL;
-            }
-        }
-    }
-    return ans;
 
 }
-
 int main() {
-    TreeNode *root = new TreeNode(1);
-//    root->left = new TreeNode(2);
-//    root->left->right = new TreeNode(3);
-//    root->right = new TreeNode(6);
-    root->right = new TreeNode(2);
-    root->right->left = new TreeNode(3);
-//    preorderTraversal(root);
-    vector<int> ans = postorderTraversal(root);
-    for (int i = 0; i < ans.size(); i++) {
-        cout << ans[i] << endl;
-    }
+    ListNode* head = new ListNode(1);
+    ListNode* sec = new ListNode(2);
+    ListNode* third = new ListNode(3);
+    ListNode* forth = new ListNode(4);
+    head->next=sec;
+    sec->next = third;
+    third->next = forth;
+    forth->next = head;
+    cout<<hasCycle(head)<<endl;
     return 0;
 }

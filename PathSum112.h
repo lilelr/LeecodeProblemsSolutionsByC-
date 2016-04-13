e73b7fd13428987e58242c016147fd9736e1f998
lilelr <lilelr@163.com>
2016-04-13 10:51:48 +0800

    112 ac

--- PathSum112.h
+++ PathSum112.h
@@ -5,4 +5,43 @@
 #ifndef LEECODEPROBLEMSSOLUTIONSBYC__PATHSUM112_H
 #define LEECODEPROBLEMSSOLUTIONSBYC__PATHSUM112_H
 
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) { }
+};
+
+void hasPathSumSec(TreeNode *root, int sum, bool &flag) {
+    if (root == nullptr) return;
+    sum -= root->val;
+    if (root->left == nullptr && root->right == nullptr) {
+        if (sum == 0) flag = true;
+    } else {
+        hasPathSumSec(root->left, sum, flag);
+        hasPathSumSec(root->right, sum, flag);
+    }
+}
+
+bool hasPathSum(TreeNode *root, int sum) {
+    if (root == nullptr) {
+        return true;
+    } else if (root->left == nullptr && root->right == nullptr) {
+        return (root->val == sum);
+    } else {
+        bool flag = false;
+        hasPathSumSec(root, sum, flag);
+        return flag;
+    }
+}
+
 #endif //LEECODEPROBLEMSSOLUTIONSBYC__PATHSUM112_H

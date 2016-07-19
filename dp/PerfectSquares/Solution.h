d3a5b135cff1730c9980d84a65eab09fbcf438b3
lilelr <lilelr@163.com>
2016-07-19 15:43:50 +0800

    279. Perfect Squares BFS 80ms

--- dp/PerfectSquares/Solution.h
+++ dp/PerfectSquares/Solution.h
@@ -47,6 +47,55 @@ public:
         }
         return cntSqures[n];
     }
+
+    //BFS 80ms
+    int numSquares3(int n) {
+        if(n <=0){
+            return 0;
+        }
+
+        vector<int> perfectSquares;
+        vector<int> cntPerfectSquares(n);
+
+        for(int i=1;i*i<=n;i++){
+            perfectSquares.push_back(i*i);
+            cntPerfectSquares[i*i-1] = 1;
+        }
+
+        if(perfectSquares.back() == n){
+            return 1;
+        }
+
+        queue<int> searchQ;
+        for(auto& item: perfectSquares){
+            searchQ.push(item);
+        }
+
+        int currCntPerfectSquares = 1;
+        while (!searchQ.empty()){
+            currCntPerfectSquares++;
+
+            int searchQSize = searchQ.size();
+            for(int i=0;i< searchQSize;i++){
+                int frontItem = searchQ.front();
+                for(auto& j:perfectSquares){
+                    if(frontItem+j == n){
+                        return currCntPerfectSquares;
+                    }
+                    else if ((frontItem+j <n) && (cntPerfectSquares[frontItem+j-1] == 0)){
+                        cntPerfectSquares[frontItem+j-1] = currCntPerfectSquares;
+                        searchQ.push(frontItem+j);
+                    }
+                    else if(frontItem+j > n){
+                        break;
+                    }
+                }
+                searchQ.pop();
+            }
+        }
+        return 0;
+    }
+
 };
 
 
--- main.cpp
+++ main.cpp
@@ -10,20 +10,50 @@ using namespace std;
 
 // 440ms
 int numSquares(int n) {
-    if (n <= 0) {
-        return 0;
+   if(n <=0){
+       return 0;
+   }
+
+    vector<int> perfectSquares;
+    vector<int> cntPerfectSquares(n);
+
+    for(int i=1;i*i<=n;i++){
+        perfectSquares.push_back(i*i);
+        cntPerfectSquares[i*i-1] = 1;
     }
 
-    static vector<int> cntSqures({0});
-    while (cntSqures.size() <= n) {
-        int curSize = cntSqures.size();
-        int newSqures = INT_MAX;
-        for (int j = 1; j * j <= curSize; j++) {
-            newSqures = min(newSqures, cntSqures[curSize - j * j] + 1);
-        }
-        cntSqures.push_back(newSqures);
+    if(perfectSquares.back() == n){
+        return 1;
     }
-    return cntSqures[n];
+
+    queue<int> searchQ;
+    for(auto& item: perfectSquares){
+        searchQ.push(item);
+    }
+
+    int currCntPerfectSquares = 1;
+    while (!searchQ.empty()){
+        currCntPerfectSquares++;
+
+        int searchQSize = searchQ.size();
+        for(int i=0;i< searchQSize;i++){
+            int frontItem = searchQ.front();
+            for(auto& j:perfectSquares){
+                if(frontItem+j == n){
+                    return currCntPerfectSquares;
+                }
+                else if ((frontItem+j <n) && (cntPerfectSquares[frontItem+j-1] == 0)){
+                    cntPerfectSquares[frontItem+j-1] = currCntPerfectSquares;
+                    searchQ.push(frontItem+j);
+                }
+                else if(frontItem+j > n){
+                    break;
+                }
+            }
+            searchQ.pop();
+        }
+        return 0;
+    }
 }
 
 int main() {

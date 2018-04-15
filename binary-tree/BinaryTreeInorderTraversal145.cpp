91989dd821d63de851d46617557dce174e06f296
Lele Li <lilelr@163.com>
2018-04-15 15:20:02 +0800

    二叉树 栈 后序遍历 中序遍历 mrrior

--- binary-tree/BinaryTreeInorderTraversal145.cpp
+++ binary-tree/BinaryTreeInorderTraversal145.cpp
@@ -29,36 +29,36 @@ void preOrder(TreeNode *root, vector<int> &ans) {
     }
 }
 
-
-vector<int> morrris_in_order_traversal(TreeNode *root) {
+// https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+vector<int> morris_in_order_tranversal(TreeNode* root){
     vector<int> ans;
-    if (root == nullptr) return ans;
-    TreeNode *cur = root;
-    while (cur != nullptr) {
-        if (cur->left == nullptr) {
+    if(root== nullptr) return ans;
+    TreeNode* cur = root;
+    while (cur!= nullptr){
+        if(cur->left== nullptr){
             ans.push_back(cur->val);
-            cur = cur->right;
-
-        } else {
-            TreeNode *predecessor = cur->left;
-            while (predecessor->right != nullptr && predecessor->right != cur) {
+            cur=cur->right;
+        }else{
+            TreeNode* predecessor = cur->left;
+            while (predecessor->right!= nullptr && predecessor->right!=cur){
                 predecessor = predecessor->right;
             }
 
-            if (predecessor->right == nullptr) {
+            if(predecessor->right == nullptr){
                 predecessor->right = cur;
                 cur = cur->left;
-            } else {
+            }
+
+            if(predecessor->right == cur){
                 predecessor->right = nullptr;
                 ans.push_back(cur->val);
                 cur = cur->right;
             }
+
         }
     }
     return ans;
 }
-
-
 int main() {
     TreeNode *root = new TreeNode(1);
 //    root->left = new TreeNode(2);
@@ -67,7 +67,7 @@ int main() {
     root->right = new TreeNode(2);
     root->right->left = new TreeNode(3);
 //    preorderTraversal(root);
-    vector<int> ans = morrris_in_order_traversal(root);
+    vector<int> ans = morris_in_order_tranversal(root);
     for (int i = 0; i < ans.size(); i++) {
         cout << ans[i] << endl;
     }

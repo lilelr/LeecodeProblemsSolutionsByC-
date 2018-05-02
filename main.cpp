db28745980f3f94721da59877e69875a2099ab54
Lele Li <lilelr@163.com>
2018-05-02 20:19:30 +0800

    逆序对,归并排序

--- main.cpp
+++ main.cpp
@@ -1,70 +1,15 @@
-/// Copyright 2016 yuXiao
-// dynamic programming
-
-#include <iostream>
-#include <vector>
-#include <stack>
-#include <queue>
-
-using namespace std;
-
-// 440ms
-int numSquares(int n) {
-   if(n <=0){
-       return 0;
-   }
-
-    vector<int> perfectSquares;
-    vector<int> cntPerfectSquares(n);
-
-    for(int i=1;i*i<=n;i++){
-        perfectSquares.push_back(i*i);
-        cntPerfectSquares[i*i-1] = 1;
-    }
-
-    if(perfectSquares.back() == n){
-        return 1;
-    }
-
-    queue<int> searchQ;
-    for(auto& item: perfectSquares){
-        searchQ.push(item);
-    }
-
-    int currCntPerfectSquares = 1;
-    while (!searchQ.empty()){
-        currCntPerfectSquares++;
-
-        int searchQSize = searchQ.size();
-        for(int i=0;i< searchQSize;i++){
-            int frontItem = searchQ.front();
-            for(auto& j:perfectSquares){
-                if(frontItem+j == n){
-                    return currCntPerfectSquares;
-                }
-                else if ((frontItem+j <n) && (cntPerfectSquares[frontItem+j-1] == 0)){
-                    cntPerfectSquares[frontItem+j-1] = currCntPerfectSquares;
-                    searchQ.push(frontItem+j);
-                }
-                else if(frontItem+j > n){
-                    break;
-                }
-            }
-            searchQ.pop();
-        }
-        return 0;
-    }
-}
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 int main() {
-//    vector<int> arr;
-//    arr.push_back(2);
-//    arr.push_back(4);
-//    arr.push_back(6);
-//    arr.push_back(9);
-//    arr.push_back(1);
-//    int res = findKthLargest(arr,5);
-    cout << numSquares(1024) << endl;
-    cout << "e" << endl;
-    return 0;
+    int time_start;
+    int fulltime = 100;//总时间
+    int runtime = 50;//运行时间
+    while (1) {
+        time_start = clock();
+        while ((clock() - time_start) < runtime) {}
+        usleep(fulltime - runtime);
+    }
 }
---
+++ sword_to_offer/sort/inverse_pairs.cpp
@@ -0,0 +1,122 @@
+//
+// Created by YuXiao on 5/2/18.
+//
+#include <string>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <cstdio>
+#include <queue>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+const int MOD = 1000000007;
+
+class Solution {
+public:
+    int inverse_pairs_core(vector<int>& data, vector<int>& cache, int start, int end) {
+        int ans = 0;
+
+        if (start == end) {
+            cache[start] = data[start];
+            return 0;
+        }
+        int mid = (end + start) / 2;
+        int left = inverse_pairs_core(cache, data, start, mid) % MOD;
+        int right = inverse_pairs_core(cache, data, mid + 1, end) % MOD;
+
+        int i = mid;
+        int j = end;
+        int cache_index = end;
+        int count = 0;
+        while (i >= start && j >= mid + 1) {
+            if (data[i] > data[j]) {
+                cache[cache_index--] = data[i--];
+                count += (j - mid)%MOD;
+                count %= MOD;
+            } else {
+                cache[cache_index--] = data[j--];
+            }
+
+        }
+        for (; i >= start; i--) {
+            cache[cache_index--] = data[i];
+        }
+        for (; j >= mid + 1; j--) {
+            cache[cache_index--] = data[j];
+        }
+
+        ans = (left + right + count) % MOD;
+
+        return ans;
+
+    }
+
+    int inverse_pairs_core2(vector<int>& data, vector<int>& cache, int start, int end) {
+        int ans = 0;
+
+        if (start == end) {
+            cache[start] = data[start];
+            return 0;
+        }
+        for(int i=start;i<=end;i++){
+            cache[i]= data[i];
+        }
+        int mid = (end + start) / 2;
+        int left = inverse_pairs_core(data, cache, start, mid) % MOD;
+        int right = inverse_pairs_core(data, cache, mid + 1, end) % MOD;
+
+        int i = mid;
+        int j = end;
+        int cache_index = end;
+        int count = 0;
+        while (i >= start && j >= mid + 1) {
+            if (cache[i] > cache[j]) {
+                data[cache_index--] = cache[i--];
+                count += (j - mid)%MOD;
+                count %= MOD;
+            } else {
+                data[cache_index--] = cache[j--];
+            }
+
+        }
+        for (; i >= start; i--) {
+            data[cache_index--] = cache[i];
+        }
+        for (; j >= mid + 1; j--) {
+            data[cache_index--] = cache[j];
+        }
+
+        ans = (left + right + count) % MOD;
+
+        return ans;
+
+    }
+
+    int InversePairs(vector<int> data) {
+        int len = data.size();
+        if (len <= 0) return 0;
+
+        vector<int> cache(data);
+//        for (int i = 0; i < len; i++) {
+//            cout << cache[i] << endl;
+//        }
+//        int res = inverse_pairs_core(data, cache, 0, len - 1);
+        int res = inverse_pairs_core2(data, cache, 0, len - 1);
+        return res;
+
+    }
+};
+
+int main() {
+    Solution s;
+  vector<int> data = {1, 2, 3, 4, 5, 6, 7, 0};
+//    vector<int> data = {1,2,3,0};
+
+//    vector<int> data = {6,7,1,0};
+    int ans = s.InversePairs(data);
+    cout << ans << endl;
+    return 0;
+}

1882517d0b779607dab63b5319f1fd7995a4235f
Lele Li <lilelr@163.com>
2018-05-14 19:45:56 +0800

    363 352

---
+++ sort/Data_Stream_as_Disjoint_Intervals_352.cpp
@@ -0,0 +1,72 @@
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <queue>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+
+// Definition for an interval.
+struct Interval {
+    int start;
+    int end;
+
+    Interval() : start(0), end(0) {}
+
+    Interval(int s, int e) : start(s), end(e) {}
+};
+
+class SummaryRanges {
+public:
+    /** Initialize your data structure here. */
+    SummaryRanges() {}
+
+    void addNum(int val) {
+        if(val ==2){
+            int tmp=2;
+            cout<<tmp<<endl;
+        }
+        auto newInterval = Interval(val, val);
+        // [2,2] lowerbound 为[., 1] upperbound为[3,.]
+        auto cmp = [](Interval a, Interval b) { return a.end + 1 < b.start; };
+        auto it0 = lower_bound(intervals.begin(), intervals.end(), newInterval, cmp);
+        auto it1 = upper_bound(intervals.begin(), intervals.end(), newInterval, cmp);
+
+        if (it0 < it1) {
+            newInterval.start = min(newInterval.start, (*it0).start);
+            newInterval.end = max(newInterval.end, (*(it1 - 1)).end);
+            intervals.erase(it0, it1);
+        }
+
+        intervals.insert(it0, newInterval);
+
+    }
+
+    vector<Interval> getIntervals() {
+        return intervals;
+    }
+
+
+private:
+    vector<Interval> intervals;
+
+
+};
+
+int main(){
+    SummaryRanges s;
+    s.addNum(1);
+    s.addNum(3);
+    s.addNum(1);
+    return 0;
+}
+/**
+ * Your SummaryRanges object will be instantiated and called as such:
+ * SummaryRanges obj = new SummaryRanges();
+ * obj.addNum(val);
+ * vector<Interval> param_2 = obj.getIntervals();
+ */

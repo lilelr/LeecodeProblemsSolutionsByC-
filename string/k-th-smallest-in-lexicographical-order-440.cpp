15a3c678590970b43441337fe95e9444145db00a
Lele Li <lilelr@163.com>
2018-05-22 09:33:38 +0800

    440. K-th Smallest in Lexicographical Order

--- string/k-th-smallest-in-lexicographical-order-440.cpp
+++ string/k-th-smallest-in-lexicographical-order-440.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <cstdio>
 #include <cmath>
+// https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/description/
 // https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/discuss/92242/ConciseEasy-to-understand-Java-5ms-solution-with-Explaination
 using namespace std;
 
@@ -15,10 +16,11 @@ public:
         k = k - 1;
         while (k > 0) {
             int steps = cal_steps(n, curr, curr + 1);
+            cout<<"K:"<<k<<" "<<"curr:"<<curr<<" steps:"<<steps<<endl;
             if (steps <= k) {
-                curr += 1;
+                curr += 1; // n1 = n2
                 k -= steps;
-            } else {
+            } else { // steps > k,
                 curr *= 10;
                 k -= 1;
             }
@@ -30,8 +32,11 @@ public:
         int steps = 0;
         while (n1 <= n) {
             steps += std::min((long)n + 1, n2) - n1;
+            cout<<"n1: "<<n1<<" n2: "<<n2<<" steps:"<<steps<<endl;
+
             n1 *= 10;
             n2 *= 10;
+
         }
         return steps;
     }
@@ -39,5 +44,8 @@ public:
 
 
 int main() {
+    Solution s;
+    int res = s.findKthNumber(13,3);
+    cout<<res<<endl;
 
 }

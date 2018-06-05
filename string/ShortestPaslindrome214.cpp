a08d3150114825bdf384c7adc86e6c47dc0884f6
Lele Li <lilelr@163.com>
2018-06-05 20:28:28 +0800

    214. Shortest Palindrome

---
+++ string/ShortestPaslindrome214.cpp
@@ -0,0 +1,60 @@
+//
+// Created by YuXiao on 3/25/
+#include <string>
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <algorithm>
+// https://leetcode.com/problems/shortest-palindrome/description/
+// https://leetcode.com/problems/shortest-palindrome/discuss/60113/Clean-KMP-solution-with-super-detailed-explanation
+// KMP 求最短回文字符串
+using namespace std;
+
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        string copy_s = s;
+        reverse(copy_s.begin(), copy_s.end());
+        string temp_str = s + '#' + copy_s;
+        vector<int> table = get_KMP_table(temp_str);
+        int table_len = temp_str.length();
+        string remain = s.substr(table[table_len -1]+1);
+        cout<<"remain "<<remain<<endl;
+        reverse(remain.begin(), remain.end());
+        string res = remain + s;
+        return res;
+    }
+
+private:
+    vector<int> get_KMP_table(string temp_str) {
+        vector<int> table(temp_str.length());
+        int j = 0, k = -1;
+        table[0] = -1;
+        while (j < temp_str.length() - 1) {
+            if (k == -1 || temp_str[k] == temp_str[j]) {
+                table[++j] = ++k;
+            } else {
+                k = table[k];
+            }
+        }
+//        for(auto& letter:temp_str){
+//            cout<<letter<<" ";
+//        }
+//        cout<<endl;
+//        for (auto& item:table){
+//            cout<<item << " ";
+//        }
+//        cout<<endl;
+        return table;
+
+    }
+};
+
+
+int main() {
+    Solution so;
+//    string str = "catacb";
+    string str1 = "aacecaaa";
+    string res = so.shortestPalindrome(str1);
+    cout<<res<<endl;
+}

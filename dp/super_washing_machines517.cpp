73812a5e2ee38af288b042781a46625a0cebcd9b
Lele Li <lilelr@163.com>
2018-06-14 20:24:48 +0800

    517. Super Washing Machines

---
+++ dp/super_washing_machines517.cpp
@@ -0,0 +1,48 @@
+//
+// Created by YuXiao on 6/14/18.
+//
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <map>
+#include <queue>
+
+using namespace std;
+// https://leetcode.com/problems/super-washing-machines/discuss/99181/C++-16ms-O(n)-solution-(with-trivial-proof)
+class Solution {
+public:
+    int findMinMoves(vector<int> &machines) {
+        int len = machines.size();
+        vector<int> sum(len + 1, 0);
+        for (int i = 0; i < len; i++) {
+            sum[i + 1] = sum[i] + machines[i];
+        }
+
+        if (sum[len] % len) return -1;
+
+        int avg = sum[len] / len;
+
+        int res = 0;
+        for (int i = 0; i < len; i++) {
+            int l = i * avg - sum[i];
+            int r = (len - i - 1) * avg - (sum[len] - sum[i] - machines[i]);
+
+            if (l > 0 && r > 0) {
+                res = max(res, abs(l) + abs(r));
+            } else {
+                res = max(res, max(abs(l), abs(r)));
+            }
+        }
+        return res;
+    }
+};
+
+
+int main() {
+
+}

c3f8a39b3b143f459d84b969cb882118618a849e
Lele Li <lilelr@163.com>
2018-10-03 09:37:01 +0800

    rand Reservoir sampling

--- binary_search/binary_search_basic.cpp
+++ binary_search/binary_search_basic.cpp
@@ -40,6 +40,30 @@ struct find_second_cmp {
     }
 };
 
+//    The idea is to do a binary search in the range from matrix[0][0] to matrix[n-1][n-1]. At each iteration, count numbers smaller than 'mid' in each row using binary search. If total count is too small, then search a larger mid, otherwise search a smaller mid.
+//
+//    The idea is not hard to understand, but the details should be clarified for you to have better understanding about binary search.
+//    (i) why use upper_bound here, not lower_bound?
+//    Ans: upper_bound find the first position where x > key, while lower_bound find the first position where x >= key. Here I want to get the count that how many entries in each row are smaller or equal to 'mid', if we use lower_bound, we will lose the part that are equal to 'mid'.
+//    (ii) why search left with condition count < k, not count <= k ?
+//    Ans: This is a common issue when it comes to binary search. Just keep in mind: when we quit the binary search, 'low' will point to the smallest position where the if-clause is not qualified (count >= k for this example here), and 'high' will point to the last position where if-clause is qualified (count < k for this example here). In this case, we want to find the smallest possible value which has count >= k, so we use (count < k) as the if-clause.
+
+
+int kthSmallest(vector<vector<int>> &matrix, int k) {
+    const int n = matrix.size();
+    int low = matrix[0][0], high = matrix[n - 1][n - 1];
+    while (low <= high) {
+        int count = 0;
+        int mid = low + (high - low) / 2;
+        for (int row = 0; row < n; row++)
+            count += upper_bound(matrix[row].begin(), matrix[row].end(), mid) - matrix[row].begin();
+        if (count < k)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return low;
+}
 
 int n;
 int q;

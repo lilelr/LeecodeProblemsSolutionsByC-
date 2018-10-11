800fc238369006494b7d9e85faf7703144f00a40
Lele Li <lilelr@163.com>
2018-10-11 09:23:50 +0800

    dp

--- dp/count_different_palindromic_subsequences_730.cpp
+++ dp/count_different_palindromic_subsequences_730.cpp
@@ -34,20 +34,20 @@ public:
             curr[ctoi(S[i])][i] = 1;
         //
         for (int len = 2; len <= n; len++) {
-            pre2 = pre1;
-            pre1 = curr;
-            for (int i = 0; i <= n - len; i++) {
+            pre2 = pre1; // pre2 记录前两轮结果 dp[char c] [index i]
+            pre1 = curr; // pre1 记录前一轮结果, curr 记录当前轮结果
+            for (int i = 0; i <= n - len; i++) { // 逐个substring 遍历，substring 长度逐渐增大
                 int sum = 2;    // aa and a, there are two at least.
                 for (int c = 0; c < C; c++) {
-                    sum += pre2[c][i + 1];
+                    sum += pre2[c][i + 1]; // 先保存pre2 长度比当前长度小2（即去掉当前substring 首尾元素）的substring 结果
                     sum %= kMod;
                 }
-                for (int c = 0; c < C; c++) {
+                for (int c = 0; c < C; c++) { // 遍历所有字母
                     if (ctoi(S[i]) != c) {
-                        curr[c][i] = pre1[c][i + 1];
-                    } else if (ctoi(S[i + len - 1]) != c) {
+                        curr[c][i] = pre1[c][i + 1]; //palindrome starting char is c
+                    } else if (ctoi(S[i + len - 1]) != c) {// 因为 pre1 的substring 长度比当前len少1
                         curr[c][i] = pre1[c][i];
-                    } else {
+                    } else { // ctoi(S[i]) == c && ctoi(S[i + len - 1]) == c
                         curr[c][i] = sum;
                     }
                 }

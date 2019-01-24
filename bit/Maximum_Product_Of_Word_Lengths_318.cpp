c3f816bbc983820d1cdca97fe803923c48671088
Lele Li <lilelr@163.com>
2019-01-24 20:55:44 +0800

    318

--- bit/Maximum_Product_Of_Word_Lengths_318.cpp
+++ bit/Maximum_Product_Of_Word_Lengths_318.cpp
@@ -14,6 +14,7 @@
 #include <numeric>      // std::accumulate
 //https://leetcode.com/problems/maximum-product-of-word-lengths/
 using namespace std;
+
 class Solution {
 public:
     bool has_common_letter(string &word1, string &word2) {
@@ -45,6 +46,21 @@ public:
         }
         return ma;
     }
+
+    int maxProduct2(vector<string> &words) {
+        vector<int> mask(words.size());
+        int result = 0;
+        for (int i = 0; i < words.size(); ++i) {
+            for (char c : words[i]) {
+                mask[i] |= 1 << (c - 'a');
+            }
+            for (int j = 0; j < i; ++j)
+                if (!(mask[i] & mask[j]))
+                    result = max(result, int(words[i].size() * words[j].size()));
+        }
+        return result;
+    }
+
 
 };
 
